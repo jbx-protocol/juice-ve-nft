3f2f2a3c97bea28914613b4fa97c2aa9e0e95e7d
tankbottoms <m@tankbottoms.xyz>
2022-05-19 23:00:35 -0400

    Added missing spreadsheet position and Forge test framework

--- contracts/JBVeTokenUriResolver.sol
+++ contracts/JBVeTokenUriResolver.sol
@@ -64,120 +64,122 @@ contract JBVeTokenUriResolver is IJBVeTokenUriResolver {
       return 2;
     } else if (_amount >= 201 && _amount <= 300) {
       return 3;
-    } else if (_amount >= 401 && _amount <= 500) {
+    } else if (_amount >= 301 && _amount <= 400) {
       return 4;
-    } else if (_amount >= 501 && _amount <= 600) {
+    } else if (_amount >= 401 && _amount <= 500) {
       return 5;
-    } else if (_amount >= 601 && _amount <= 700) {
+    } else if (_amount >= 501 && _amount <= 600) {
       return 6;
-    } else if (_amount >= 701 && _amount <= 800) {
+    } else if (_amount >= 601 && _amount <= 700) {
       return 7;
-    } else if (_amount >= 801 && _amount <= 900) {
+    } else if (_amount >= 701 && _amount <= 800) {
       return 8;
-    } else if (_amount >= 901 && _amount <= 1000) {
+    } else if (_amount >= 801 && _amount <= 900) {
       return 9;
-    } else if (_amount >= 1001 && _amount <= 2000) {
+    } else if (_amount >= 901 && _amount <= 1000) {
       return 10;
-    } else if (_amount >= 2001 && _amount <= 3000) {
+    } else if (_amount >= 1001 && _amount <= 2000) {
       return 11;
-    } else if (_amount >= 3001 && _amount <= 4000) {
+    } else if (_amount >= 2001 && _amount <= 3000) {
       return 12;
-    } else if (_amount >= 4001 && _amount <= 5000) {
+    } else if (_amount >= 3001 && _amount <= 4000) {
       return 13;
-    } else if (_amount >= 5001 && _amount <= 6000) {
+    } else if (_amount >= 4001 && _amount <= 5000) {
       return 14;
-    } else if (_amount >= 6001 && _amount <= 7000) {
+    } else if (_amount >= 5001 && _amount <= 6000) {
       return 15;
-    } else if (_amount >= 7001 && _amount <= 8000) {
+    } else if (_amount >= 6001 && _amount <= 7000) {
       return 16;
-    } else if (_amount >= 8001 && _amount <= 9000) {
+    } else if (_amount >= 7001 && _amount <= 8000) {
       return 17;
-    } else if (_amount >= 9001 && _amount <= 10000) {
+    } else if (_amount >= 8001 && _amount <= 9000) {
       return 18;
-    } else if (_amount >= 10001 && _amount <= 12000) {
+    } else if (_amount >= 9001 && _amount <= 10000) {
       return 19;
-    } else if (_amount >= 12001 && _amount <= 14000) {
+    } else if (_amount >= 10001 && _amount <= 12000) {
       return 20;
-    } else if (_amount >= 14001 && _amount <= 16000) {
+    } else if (_amount >= 12001 && _amount <= 14000) {
       return 21;
-    } else if (_amount >= 16001 && _amount <= 18000) {
+    } else if (_amount >= 14001 && _amount <= 16000) {
       return 22;
-    } else if (_amount >= 18001 && _amount <= 20000) {
+    } else if (_amount >= 16001 && _amount <= 18000) {
       return 23;
-    } else if (_amount >= 20001 && _amount <= 22000) {
+    } else if (_amount >= 18001 && _amount <= 20000) {
       return 24;
-    } else if (_amount >= 22001 && _amount <= 24000) {
+    } else if (_amount >= 20001 && _amount <= 22000) {
       return 25;
-    } else if (_amount >= 24001 && _amount <= 26000) {
+    } else if (_amount >= 22001 && _amount <= 24000) {
       return 26;
-    } else if (_amount >= 26001 && _amount <= 28000) {
+    } else if (_amount >= 24001 && _amount <= 26000) {
       return 27;
-    } else if (_amount >= 28001 && _amount <= 30000) {
+    } else if (_amount >= 26001 && _amount <= 28000) {
       return 28;
-    } else if (_amount >= 30001 && _amount <= 40000) {
+    } else if (_amount >= 28001 && _amount <= 30000) {
       return 29;
-    } else if (_amount >= 40001 && _amount <= 50000) {
+    } else if (_amount >= 30001 && _amount <= 40000) {
       return 30;
-    } else if (_amount >= 50001 && _amount <= 60000) {
+    } else if (_amount >= 40001 && _amount <= 50000) {
       return 31;
-    } else if (_amount >= 60001 && _amount <= 70000) {
+    } else if (_amount >= 50001 && _amount <= 60000) {
       return 32;
-    } else if (_amount >= 70001 && _amount <= 80000) {
+    } else if (_amount >= 60001 && _amount <= 70000) {
       return 33;
-    } else if (_amount >= 80001 && _amount <= 90000) {
+    } else if (_amount >= 70001 && _amount <= 80000) {
       return 34;
-    } else if (_amount >= 90001 && _amount <= 100000) {
+    } else if (_amount >= 80001 && _amount <= 90000) {
       return 35;
-    } else if (_amount >= 100001 && _amount <= 200000) {
+    } else if (_amount >= 90001 && _amount <= 100000) {
       return 36;
-    } else if (_amount >= 200001 && _amount <= 300000) {
+    } else if (_amount >= 100001 && _amount <= 200000) {
       return 37;
-    } else if (_amount >= 300001 && _amount <= 400000) {
+    } else if (_amount >= 200001 && _amount <= 300000) {
       return 38;
-    } else if (_amount >= 400001 && _amount <= 500000) {
+    } else if (_amount >= 300001 && _amount <= 400000) {
       return 39;
-    } else if (_amount >= 500001 && _amount <= 600000) {
+    } else if (_amount >= 400001 && _amount <= 500000) {
       return 40;
-    } else if (_amount >= 600001 && _amount <= 700000) {
+    } else if (_amount >= 500001 && _amount <= 600000) {
       return 41;
-    } else if (_amount >= 700001 && _amount <= 800000) {
+    } else if (_amount >= 600001 && _amount <= 700000) {
       return 42;
-    } else if (_amount >= 800001 && _amount <= 900000) {
+    } else if (_amount >= 700001 && _amount <= 800000) {
       return 43;
-    } else if (_amount >= 900001 && _amount <= 1000000) {
+    } else if (_amount >= 800001 && _amount <= 900000) {
       return 44;
-    } else if (_amount >= 1000001 && _amount <= 2000000) {
+    } else if (_amount >= 900001 && _amount <= 1000000) {
       return 45;
-    } else if (_amount >= 2000001 && _amount <= 3000000) {
+    } else if (_amount >= 1000001 && _amount <= 2000000) {
       return 46;
-    } else if (_amount >= 3000001 && _amount <= 4000000) {
+    } else if (_amount >= 2000001 && _amount <= 3000000) {
       return 47;
-    } else if (_amount >= 4000001 && _amount <= 5000000) {
+    } else if (_amount >= 3000001 && _amount <= 4000000) {
       return 48;
-    } else if (_amount >= 5000001 && _amount <= 6000000) {
+    } else if (_amount >= 4000001 && _amount <= 5000000) {
       return 49;
-    } else if (_amount >= 6000001 && _amount <= 7000000) {
+    } else if (_amount >= 5000001 && _amount <= 6000000) {
       return 50;
-    } else if (_amount >= 7000001 && _amount <= 8000000) {
+    } else if (_amount >= 6000001 && _amount <= 7000000) {
       return 51;
-    } else if (_amount >= 8000001 && _amount <= 9000000) {
+    } else if (_amount >= 7000001 && _amount <= 8000000) {
       return 52;
-    } else if (_amount >= 9000001 && _amount <= 10000000) {
+    } else if (_amount >= 8000001 && _amount <= 9000000) {
       return 53;
-    } else if (_amount >= 10000001 && _amount <= 20000000) {
+    } else if (_amount >= 9000001 && _amount <= 10000000) {
       return 54;
-    } else if (_amount >= 20000001 && _amount <= 40000000) {
+    } else if (_amount >= 10000001 && _amount <= 20000000) {
       return 55;
-    } else if (_amount >= 40000001 && _amount <= 50000000) {
+    } else if (_amount >= 20000001 && _amount <= 40000000) {
       return 56;
-    } else if (_amount >= 50000001 && _amount <= 100000000) {
+    } else if (_amount >= 40000001 && _amount <= 50000000) {
       return 57;
-    } else if (_amount >= 100000001 && _amount <= 500000000) {
+    } else if (_amount >= 50000001 && _amount <= 100000000) {
       return 58;
-    } else if (_amount >= 500000001 && _amount <= 700000000) {
+    } else if (_amount >= 100000001 && _amount <= 500000000) {
       return 59;
-    } else {
+    } else if (_amount >= 500000001 && _amount <= 700000000) {
       return 60;
+    } else {
+      return 61;
     }
   }
 
---
+++ contracts/test/TestJBVeTokenUriResolver.sol
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import './helpers/TestBaseWorkflow.t.sol';
+import '../interfaces/IJBVeTokenUriResolver.sol';
+
+contract JBVeVeTokenUriResolverTests is TestBaseWorkflow {
+  //*********************************************************************//
+  // --------------------- private stored properties ------------------- //
+  //*********************************************************************//
+  JBveBanny private _jbveBanny;
+  JBVeTokenUriResolver private _jbveTokenUriResolver;
+  JBTokenStore private _jbTokenStore;
+  JBController private _jbController;
+  JBOperatorStore private _jbOperatorStore;
+  uint256 private _projectId;
+  address private _projectOwner;
+
+  //*********************************************************************//
+  // --------------------------- test setup ---------------------------- //
+  //*********************************************************************//
+  function setUp() public override {
+    // calling before each for TestBaseWorkflow
+    super.setUp();
+    // fetching instances deployed in the base workflow file
+    _projectId = projectID();
+    _jbTokenStore = jbTokenStore();
+    _jbOperatorStore = jbOperatorStore();
+    _jbveTokenUriResolver = jbveTokenUriResolver();
+    _jbController = jbController();
+
+    // lock duration options array to be used for mock deployment
+    uint256[] memory _lockDurationOptions = new uint256[](3);
+    _lockDurationOptions[0] = 864000;
+    _lockDurationOptions[1] = 2160000;
+    _lockDurationOptions[2] = 8640000;
+
+    // JBveBanny
+    _jbveBanny = new JBveBanny(
+      _projectId,
+      'Banny',
+      'Banny',
+      IJBVeTokenUriResolver(address(_jbveTokenUriResolver)),
+      IJBTokenStore(address(_jbTokenStore)),
+      IJBOperatorStore(address(_jbOperatorStore)),
+      _lockDurationOptions
+    );
+  }
+
+  function testConstructor() public {
+    uint256[] memory _lockDurationOptions = new uint256[](3);
+    _lockDurationOptions[0] = 864000;
+    _lockDurationOptions[1] = 2160000;
+    _lockDurationOptions[2] = 8640000;
+    // assertion checks for constructor code
+    assertEq(address(_jbTokenStore.tokenOf(_projectId)), address(_jbveBanny.token()));
+    assertEq(address(_jbveTokenUriResolver), address(_jbveBanny.uriResolver()));
+    assertEq(_projectId, _jbveBanny.projectId());
+    assertEq(_lockDurationOptions[0], _jbveBanny.lockDurationOptions()[0]);
+  }
+
+  function mintIJBTokens() public returns (IJBToken) {
+    IJBToken _token = _jbTokenStore.tokenOf(_projectId);
+    _projectOwner = projectOwner();
+    evm.startPrank(_projectOwner);
+    _jbController.mintTokensOf(_projectId, 100 ether, _projectOwner, 'Test Memo', true, true);
+    _token.approve(_projectId, address(_jbveBanny), 10 ether);
+    return _token;
+  }
+
+  function testLockWithJBToken() public {
+    mintIJBTokens();
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    assertEq(_jbveBanny.ownerOf(1), _projectOwner);
+    (uint256 amount, uint256 duration, , bool isJbToken,) = _jbveBanny.getSpecs(1);
+    assertEq(amount, 10 ether);
+    assertEq(duration, 864000);
+    assert(isJbToken);
+  }
+
+  function testUnlockingTokens() public {
+    IJBToken _token = mintIJBTokens();
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    (, , uint256 lockedUntil, ,) = _jbveBanny.getSpecs(1);
+    evm.warp(lockedUntil * 2);
+    _jbveBanny.approve(address(_jbveBanny), 1);
+    JBUnlockData[] memory unlocks = new JBUnlockData[](1);
+    unlocks[0] = JBUnlockData(
+      1, _projectOwner
+    );
+    _jbveBanny.unlock(unlocks);
+    assertEq(_token.balanceOf(address(_jbveBanny), _projectId), 0);
+  }
+
+  function testExtendLock() public {
+    mintIJBTokens();
+    uint256 _tokenId = _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    (, uint256 d, uint256 lockedUntil, ,) = _jbveBanny.getSpecs(_tokenId);
+    assertEq(d, 864000);
+    evm.warp(lockedUntil * 2);
+
+    JBLockExtensionData[] memory extends = new JBLockExtensionData[](1);
+    extends[0] = JBLockExtensionData(
+      1, 8640000
+    );
+    _tokenId = _jbveBanny.extendLock(extends)[0];
+
+    (, uint256 duration, , ,) = _jbveBanny.getSpecs(_tokenId);
+    assertEq(duration, 8640000);
+  }
+
+  function testScenarioWithInvalidLockDuration() public {
+    mintIJBTokens();
+    evm.expectRevert(abi.encodeWithSignature('INVALID_LOCK_DURATION()'));
+    _jbveBanny.lock(_projectOwner, 10 ether, 864001, _projectOwner, true, false);
+  }
+
+  function testScenarioWithInsufficientBalance() public {
+    mintIJBTokens();
+    evm.expectRevert(abi.encodeWithSignature('INSUFFICIENT_BALANCE()'));
+    _jbveBanny.lock(_projectOwner, 101 ether, 864000, _projectOwner, true, false);
+  }
+
+  function testScenarioWhenLockPeriodIsNotOver() public {
+    mintIJBTokens();
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    (, , uint256 lockedUntil,,) = _jbveBanny.getSpecs(1);
+    evm.warp(lockedUntil / 2);
+    _jbveBanny.approve(address(_jbveBanny), 1);
+    evm.expectRevert(abi.encodeWithSignature('LOCK_PERIOD_NOT_OVER()'));
+    JBUnlockData[] memory unlocks = new JBUnlockData[](1);
+    unlocks[0] = JBUnlockData(
+      1, _projectOwner
+    );
+    _jbveBanny.unlock(unlocks);
+  }
+
+  function testScenarioWithInvalidLockDurationWhenExtendingDuration() public {
+    mintIJBTokens();
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    (, uint256 d, uint256 lockedUntil,,) = _jbveBanny.getSpecs(1);
+    assertEq(d, 864000);
+    evm.warp(lockedUntil * 2);
+    evm.expectRevert(abi.encodeWithSignature('INVALID_LOCK_DURATION()'));
+
+    JBLockExtensionData[] memory extends = new JBLockExtensionData[](1);
+    extends[0] = JBLockExtensionData(
+      1, 8640001
+    );
+    _jbveBanny.extendLock(extends);
+  }
+
+  function testScenarioWithInvalidLockExtension() public {
+    mintIJBTokens();
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, true, false);
+    (, uint256 d, uint256 lockedUntil,,) = _jbveBanny.getSpecs(1);
+    assertEq(d, 864000);
+    evm.warp(lockedUntil / 2);
+    evm.expectRevert(abi.encodeWithSignature('INVALID_LOCK_EXTENSION()'));
+
+    JBLockExtensionData[] memory extends = new JBLockExtensionData[](1);
+    extends[0] = JBLockExtensionData(
+      1, 8640000
+    );
+    _jbveBanny.extendLock(extends);
+  }
+
+  function testLockWithNonJbToken() public {
+    _projectOwner = projectOwner();
+    evm.startPrank(_projectOwner);
+    _jbController.mintTokensOf(_projectId, 100 ether, _projectOwner, 'Test Memo', false, true);
+    uint256[] memory _permissionIndexes = new uint256[](1);
+    _permissionIndexes[0] = JBOperations.TRANSFER;
+    jbOperatorStore().setOperator(
+      JBOperatorData(address(_jbveBanny), _projectId, _permissionIndexes)
+    );
+    _jbveBanny.lock(_projectOwner, 10 ether, 864000, _projectOwner, false, false);
+    assertEq(_jbveBanny.ownerOf(1), _projectOwner);
+    (uint256 amount, uint256 duration, , ,) = _jbveBanny.getSpecs(1);
+    assertEq(amount, 10 ether);
+    assertEq(duration, 864000);
+  }
+}
