145b66c003533f7e09d87d90feaa4cb0a60d36a8
xBA5ED <0xBA5ED@protonmail.com>
2022-06-14 00:58:03 +0200

    Increase operation IDs so we don't clash with  jb-project-handles

--- contracts/libraries/JBStakingOperations.sol
+++ contracts/libraries/JBStakingOperations.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.6;
 
 library JBStakingOperations {
-  uint256 public constant DEPLOY_VE_NFT = 19;
-  uint256 public constant LOCK = 20;
-  uint256 public constant UNLOCK = 21;
-  uint256 public constant EXTEND_LOCK = 22;
-  uint256 public constant REDEEM = 23;
-  uint256 public constant SET_PUBLIC_EXTENSION_FLAG = 24;
+  uint256 public constant DEPLOY_VE_NFT = 20;
+  uint256 public constant LOCK = 21;
+  uint256 public constant UNLOCK = 22;
+  uint256 public constant EXTEND_LOCK = 23;
+  uint256 public constant REDEEM = 24;
+  uint256 public constant SET_PUBLIC_EXTENSION_FLAG = 25;
 }
